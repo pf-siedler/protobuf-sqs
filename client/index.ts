eab6e532419a1a1eb26bcf3ae823e59e310303f8
Takuma Suzuki <pferd262@gmail.com>
2021-03-02 17:49:35 +0900

    :recycle: Refactor client and consumer

--- client/index.ts
+++ client/index.ts
@@ -2,41 +2,45 @@ import { IPurchase, Purchase } from "../generated";
 import { SQS } from "aws-sdk";
 import { DropUndefined } from "./types";
 
-function main() {
-    // 適当に環境変数を読み取るゾーン
-    const region = process.env.AWS_REGION ?? "ap-northeast-1";
+async function main(): Promise<void> {
     const QueueUrl = process.env.QUEUE_URL;
-
     if (QueueUrl === undefined) {
         console.error("Please set env `QUEUE_URL`");
         process.exit(1);
     }
+    const region = process.env.AWS_REGION ?? "ap-northeast-1";
 
+    // TS の object
     const rawMessage: DropUndefined<IPurchase> = {
-        productId: "abc",
-        userId: "123",
-        price: 123.4,
-        timestamp: Math.round(new Date(0).getTime() / 1000),
+        userId: "hoge",
+        items: [{ productId: "fuga", price: 123.4 }],
+        timestamp: new Date(0).valueOf(),
     };
 
-    // Protobuf.js の Message Class
+    // Protobuf.js が生成した Class
     const message: Purchase = Purchase.create(rawMessage);
-    // Protobuf の仕様に従って serialize されたバイト列
-    const encoded: Uint8Array = Purchase.encode(message).finish();
+
+    // serialize されたバイト列
+    const serialized: Uint8Array = Purchase.encode(message).finish();
+
     // バイト列を base64 でエンコードして文字列にしたもの
-    const base64ed: string = Buffer.from(encoded).toString("base64");
+    const base64String: string = Buffer.from(serialized).toString("base64");
 
     // SQS に積む処理
-    new SQS({ region }).sendMessage(
-        {
-            MessageBody: base64ed,
+    await new SQS({ region })
+        .sendMessage({
+            MessageBody: base64String,
             QueueUrl,
-        },
-        (err, data) => {
-            console.error(err);
-            console.log(data);
-        },
-    );
+        })
+        .promise();
 }
 
-main();
+main()
+    .catch(e => {
+        console.error(e);
+        process.exit(1);
+    })
+    .then(() => {
+        console.log("finished");
+        process.exit(0);
+    });
--- consumer/index.ts
+++ consumer/index.ts
@@ -1,38 +1,80 @@
 import { SQS } from "aws-sdk";
 import { Purchase } from "../generated";
 
-function main() {
-    // 適当に環境変数を読み取るゾーン
-    const region = process.env.AWS_REGION ?? "ap-northeast-1";
+async function main() {
     const QueueUrl = process.env.QUEUE_URL;
-
     if (QueueUrl === undefined) {
         console.error("Please set env `QUEUE_URL`");
         process.exit(1);
     }
+    const region = process.env.AWS_REGION ?? "ap-northeast-1";
 
-    const sqs = new SQS({ region });
-    // SQS からメッセージを pop する
-    // 今回は第2引数に cb を渡す方法で書いたが、 `sqs.receiveMessage({ QueueUrl }).promise()` で async/await 使う方が書きやすい
-    sqs.receiveMessage({ QueueUrl }, (err, data) => {
-        console.error(err);
-        // バイト列を base64 でエンコードして文字列にしたもの
-        const base64ed = data.Messages![0].Body;
-        if (base64ed === undefined) {
-            console.error("failed to receive message");
-            process.exit(1);
-        }
-        // Protobuf の仕様に従って serialize されたバイト列
-        const encoded = Uint8Array.from(Buffer.from(base64ed, "base64"));
-        // Protobuf.js の Message Class
-        const message = Purchase.decode(encoded);
-
-        // `message.productId` や `message.price` 等で各プロパティにアクセスできる
-        // 型もちゃんと付いている
-
-        // 雑に JSON に変換して標準出力に吐く
-        console.log(JSON.stringify(message.toJSON()));
+    const sqsInstance = new SQS({
+        region,
     });
+
+    // SQS からメッセージを取り出す
+    const sqsResult = await sqsInstance
+        .receiveMessage({
+            QueueUrl,
+        })
+        .promise();
+    if (sqsResult.Messages === undefined || sqsResult.Messages.length === 0) {
+        console.log("no messages found");
+        process.exit(0);
+    }
+
+    // base64 string から Protobuf.js の Message Class を生成する
+    const getMessage = (base64String: string): Purchase => {
+        // Protobuf の仕様に従って serialize されたバイト列
+        const serialized = Uint8Array.from(Buffer.from(base64String, "base64"));
+        // Protobuf.js の Message Class
+        return Purchase.decode(serialized);
+    };
+    const messages = sqsResult.Messages.map(m => m.Body)
+        .filter(<T>(x: T | undefined): x is T => x !== undefined)
+        .map(str => getMessage(str));
+
+    // 各 message について、何らかの処理を行う
+    // 今回は console.log に適当に文を吐くだけ
+    messages.forEach(message => {
+        // `message.userId` 等でプロパティにアクセスできる
+        const userId = message.userId;
+        // 型も付く Protobuf で `uint32 timestamp = 3;` と定義したので、`message.timestamp` は number 型
+        const date = new Date(message.timestamp);
+
+        // ただし、 Message をネストすると型が正しく付かない
+        // message.items[n].price は `number | null | undefined`
+        const sumPrice = message.items.reduce(
+            (sum, i) => sum + (i.price ?? 0),
+            0,
+        );
+
+        console.log(
+            `ユーザー（${userId}）が ${date.toLocaleDateString()} に合計 ${Math.floor(
+                sumPrice,
+            )}円の買い物をしました`,
+        );
+    });
+
+    // 取得したメッセージを消す
+    await sqsInstance
+        .deleteMessageBatch({
+            QueueUrl,
+            Entries: sqsResult.Messages.map(m => ({
+                Id: m.MessageId!,
+                ReceiptHandle: m.ReceiptHandle!,
+            })),
+        })
+        .promise();
 }
 
-main();
+main()
+    .catch(e => {
+        console.error(e);
+        process.exit(1);
+    })
+    .then(() => {
+        console.log("finished");
+        process.exit(0);
+    });
